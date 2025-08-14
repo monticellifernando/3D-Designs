85425b5013a1aa0860a37a6fbd80c28871b07d0d
fernando <Fernando.Monticelli@cern.ch>
2025-08-14 10:06:01 -0300

    LabJack

--- Herramientas/LabJack/Presupuesto.sh
+++ Herramientas/LabJack/Presupuesto.sh
@@ -6,3 +6,14 @@
 # Diseño, 15000
 # Materiales, 5000
 # Total: 48750 
+#
+# Solo impresión:
+# 
+# ~/Documents/fernando/Proyectos/Costo3DPrints.py -t 6 -u 1 -g 1 -p 0.16
+# Impresión de 6.0 horas, Ganancia 750 unidades en 240 horas por mes. Valor de la unidad = 1.0:
+# Maquina :  18.75
+# Filamento :  2.72
+# Ganancia :  21.47
+# -----------------
+# Valor 42.94
+
